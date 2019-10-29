45b726e708b664d9615a3691067c9f6c2bc3ad4f
Odlopez <Odlopez@inbox.ru>
2019-10-29 21:13:54 +0300

    Поправляет синтаксис

--- functions.php
+++ functions.php
@@ -54,12 +54,11 @@ function getAllTasks()
 
 /**
  * Возвращает количество совпадающих по назанию категорий в переданном массиве проектов
- *
  * @param array $projects_list массив с проектами
  * @param string $name имя категории
  * @return int количество сопадающих категорий
  */
-function counts_category_in_projects(array $projects_list, string $name) : int
+function counts_category_in_projects(array $projects_list, string $name): int
 {
     return array_reduce($projects_list, function ($carry, $item_project) use ($name) {
         $carry += $item_project['category'] === $name ? 1 : 0;
