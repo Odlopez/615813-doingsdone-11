d9356afe75bc81b4504fae8da1b20c65c48d6d32
Odlopez <Odlopez@inbox.ru>
2019-11-07 18:47:22 +0300

    Убирает значение по-умолчанию у даты задачи

--- scheme.sql
+++ scheme.sql
@@ -21,7 +21,7 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     project_id INT NOT NULL,
-    date_task TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_task TIMESTAMP,
     file VARCHAR(255),
     is_done TINYINT
 );
